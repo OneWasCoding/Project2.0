1b9a09e85df899ca25301bde44f5b695dba2fa5d
KoolvSter <ianzae123ego@gmail.com>
2024-11-05 14:07:37 +0800

    Update manage_products.php

--- admin/manage_products.php
+++ admin/manage_products.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
-    header("Location: ../login.php");
+    header("Location: ../admin/manage_products/");
     exit();
 }
-include '../db/db.php';
+include '../includes/db.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Add or update product
