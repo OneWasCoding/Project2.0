e7f724b9d42d2ff0aac14715d25521f81b7041aa
KoolvSter <ianzae123ego@gmail.com>
2024-11-05 14:14:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="EM’ Quality Shoes - Your destination for quality footwear.">
     <link rel="stylesheet" href="assets/css/style.css">
     <title>EM’ Quality Shoes</title>
 </head>
